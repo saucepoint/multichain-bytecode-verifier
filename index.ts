f359e8f648319bada3c1cab25fe4c27ef4585137
saucepoint <saucepoint@protonmail.com>
2023-09-12 15:48:51 -0400

    hardcoded example

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import { markdownTable } from "markdown-table";
+import { clients, mainnetClient } from "./utils/chains";
+
+const address = "0x4e59b44847b379578588920cA78FbF26c0B4956C";
+
+const mainnetBytecode = await mainnetClient.getBytecode({
+  address,
+});
+
+const data = await Promise.all(
+  clients.map(async (client) => {
+    return {
+      chain: client.chain!.name,
+      bytecode: await client.getBytecode({
+        address,
+      }),
+    };
+  })
+);
+
+const match = data.map((d) => {
+  return [d.chain, d.bytecode === mainnetBytecode ? "✅" : "❌"];
+});
+
+console.log(`Address: ${address}`);
+console.log(`Bytecode: ${mainnetBytecode}`);
+console.log("");
+console.log(markdownTable([["Chain", "Matching Bytecode"], ...match]));
