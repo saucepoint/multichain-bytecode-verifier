f651e8d338f862b8497ea7a3b0643a2db73741c2
saucepoint <saucepoint@protonmail.com>
2023-09-12 15:55:33 -0400

    commandline arguments

--- index.ts
+++ index.ts
@@ -1,12 +1,28 @@
+import { program } from "commander";
 import { markdownTable } from "markdown-table";
 import { clients, mainnetClient } from "./utils/chains";
 
-const address = "0x4e59b44847b379578588920cA78FbF26c0B4956C";
+program.description(
+  "Compare bytecode of a mainnet contract on different chains. Useful for verifying that a contract address is ~determinisitically reusable."
+);
 
+program.argument(
+  "<address>",
+  "Ethereum mainnet contract address. This address's bytecode will be compared on other chains."
+);
+
+program.parse();
+
+// -----------------------------------------------
+
+const address = program.args[0] as `0x${string}`;
+
+// get mainnet bytecode
 const mainnetBytecode = await mainnetClient.getBytecode({
   address,
 });
 
+// fetch bytecode from all other chains defined in utils/chains.ts
 const data = await Promise.all(
   clients.map(async (client) => {
     return {
@@ -18,10 +34,12 @@ const data = await Promise.all(
   })
 );
 
+// compare bytecode against mainnet
 const match = data.map((d) => {
   return [d.chain, d.bytecode === mainnetBytecode ? "✅" : "❌"];
 });
 
+// results and reporting
 console.log(`Address: ${address}`);
 console.log(`Bytecode: ${mainnetBytecode}`);
 console.log("");
